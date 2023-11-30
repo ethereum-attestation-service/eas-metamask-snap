539f8906a1097a9e90702176c79409daea1d8526
Steve Dakh <slavik0329@gmail.com>
2023-11-30 11:33:24 -0500

    fix lint errors

--- packages/snap/src/index.test.ts
+++ packages/snap/src/index.test.ts
@@ -1,10 +1,8 @@
 import { expect } from '@jest/globals';
-import { installSnap } from '@metamask/snaps-jest';
-import { panel, text } from '@metamask/snaps-sdk';
 
 describe('onRpcRequest', () => {
   describe('Initialize', () => {
-    it('Simple test', async () => {
+    it('simple test', async () => {
       expect(1).toBe(1);
     });
   });
